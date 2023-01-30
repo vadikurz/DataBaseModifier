7eb64035678586966e9eab67abc3f2d3a8d9bff0
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-01-30 15:09:55 +0300

    change logic of reader and writer

--- DataBaseModifier/Program.cs
+++ DataBaseModifier/Program.cs
@@ -5,25 +5,29 @@ namespace DataBaseModifier;
 public class Program
 {
     private const string OutputPrefix = "modified";
-    
+
     /// <param name="args">args[0] is the path to file, args[1] is the file name</param>
     public static void Main(string[] args)
     {
-        var records = CsvFileReader<OriginalVievModel>.Read(Path.Combine(args), ",");
-        
-        CsvFileWriter<SubViewModel>.Write(Path.Combine(args[0], OutputPrefix + args[1]), ",", records
+        var reader = new CsvFileReader(Path.Combine(args), ',');
+        var writer = new CsvFileWriter(Path.Combine(args[0], OutputPrefix + args[1]), ',');
+
+        var records = reader.Read();
+
+        var gsmRecords = records
             .Where(record => record.Radio == "GSM")
             .Select(
                 record =>
                     new SubViewModel
                     {
-                        Radio = record.Radio,
                         MCC = record.MCC,
                         MNC = record.MNC,
                         LAC_TAC_NID = record.LAC_TAC_NID,
                         CID = record.CID,
                         LongitudeEW = record.LongitudeEW,
                         LongitudeNS = record.LongitudeNS
-                    }));
+                    });
+
+        writer.Write(gsmRecords);
     }
 }
