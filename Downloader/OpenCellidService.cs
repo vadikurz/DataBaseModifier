76a59b07d94ea60a5e9abc92b7b760872e6c2f1d
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-02-06 12:58:54 +0300

    refactor methods

---
+++ Downloader/OpenCellidService.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Compression;
+
+namespace Downloader;
+
+public sealed class OpenCellidService : IDisposable
+{
+    private readonly HttpClient client = new();
+
+    public string Token { get; set; } = "pk.f48f741b488ae7c9a645c3fb04171bef";
+
+    public async Task DownloadAndDecompressAsync(string fileName, string path)
+    {
+        if (!fileName.EndsWith(".gz"))
+        {
+            throw new ArgumentException("Expected (*.gz)", nameof(fileName));
+        }
+        
+        var url = $"https://opencellid.org/ocid/downloads?token={Token}&type=mcc&file={fileName}";
+
+        await using var stream = await client.GetStreamAsync(url);
+        await using var decompressionStream = new GZipStream(stream, CompressionMode.Decompress);
+        await using var targetStream = File.Create(Path.Combine(path ,fileName[..^".gz".Length]));
+        await decompressionStream.CopyToAsync(targetStream);
+    }
+
+    public void Dispose() => client.Dispose();
+}
--- Downloader/Program.cs
+++ Downloader/Program.cs
@@ -5,7 +5,15 @@ public class Program
     /// <param name="args">args[0] is the output directory for file</param>
     public static async Task Main(string[] args)
     {
-        var service = new OpenCelidService(args[0]);
-        await service.DownloadAsync();
+        var path = args.Any() ? args[0] : Environment.CurrentDirectory;
+
+        if (!Directory.Exists(path))
+        {
+            Console.WriteLine("Usage: Downloader.exe [<output dir>]");
+            Environment.Exit(-1);
+        }
+        
+        using var service = new OpenCellidService();
+        await service.DownloadAndDecompressAsync("257.csv.gz", path);
     }
 }
