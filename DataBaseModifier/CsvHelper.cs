f0551fc72ae9a206ae819a850b10a3155f988ca2
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-02-08 12:34:40 +0300

    refactor methods

--- DataBaseModifier/CsvHelper.cs
+++ DataBaseModifier/CsvHelper.cs
@@ -6,19 +6,20 @@ namespace DataBaseModifier;
 
 public class CsvHelper
 {
-    public void ReadAndWrite(GZipStream stream,string originalFileOutputPath,string modifiedFileOutputPath,  char delimiter)
-    { 
+    public void ReadAndWrite(GZipStream stream, string originalFileOutputPath, string modifiedFileOutputPath,
+        char delimiter)
+    {
         CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
-        
+
         using var streamReader = new StreamReader(stream);
         using var modifiedFileStream = File.OpenWrite(modifiedFileOutputPath);
         using var originalFileStream = File.OpenWrite(originalFileOutputPath);
         using var modifiedStreamWriter = new StreamWriter(modifiedFileStream);
         using var originalStreamWriter = new StreamWriter(originalFileStream);
-        
+
         string line;
         var resultLine = new StringBuilder();
-        
+
         int MCC;
         int MNC;
         int LAC_TAC_NID;
@@ -29,9 +30,9 @@ public class CsvHelper
         while ((line = streamReader.ReadLine()) != null)
         {
             originalStreamWriter.WriteLine(line);
-            
+
             resultLine.Clear();
-            
+
             var firstDelimiter = line.IndexOf(delimiter);
 
             if (line[..firstDelimiter] != "GSM")
@@ -40,24 +41,25 @@ public class CsvHelper
             }
 
             ReadOnlySpan<char> buffer = line.AsSpan();
-            
+
             var indexOf2Delimiter = GetIndexOfDelimiter(buffer, firstDelimiter, delimiter);
-            
+
             int.TryParse(buffer.Slice(firstDelimiter + 1, indexOf2Delimiter - firstDelimiter - 1), out MCC);
-            
+
             resultLine.Append(MCC);
             resultLine.Append(delimiter);
-            
+
             var indexOf3Delimiter = GetIndexOfDelimiter(buffer, indexOf2Delimiter, delimiter);
 
             int.TryParse(buffer.Slice(indexOf2Delimiter + 1, indexOf3Delimiter - indexOf2Delimiter - 1), out MNC);
-            
+
             resultLine.Append(MNC);
             resultLine.Append(delimiter);
-            
+
             var indexOf4Delimiter = GetIndexOfDelimiter(buffer, indexOf3Delimiter, delimiter);
 
-            int.TryParse(buffer.Slice(indexOf3Delimiter + 1, indexOf4Delimiter - indexOf3Delimiter - 1), out LAC_TAC_NID);
+            int.TryParse(buffer.Slice(indexOf3Delimiter + 1, indexOf4Delimiter - indexOf3Delimiter - 1),
+                out LAC_TAC_NID);
 
             resultLine.Append(LAC_TAC_NID);
             resultLine.Append(delimiter);
@@ -65,23 +67,25 @@ public class CsvHelper
             var indexOf5Delimiter = GetIndexOfDelimiter(buffer, indexOf4Delimiter, delimiter);
 
             int.TryParse(buffer.Slice(indexOf4Delimiter + 1, indexOf5Delimiter - indexOf4Delimiter - 1), out CID);
-            
+
             resultLine.Append(CID);
             resultLine.Append(delimiter);
-            
+
             var indexOf6Delimiter = GetIndexOfDelimiter(buffer, indexOf5Delimiter, delimiter);
-            
+
             var indexOf7Delimiter = GetIndexOfDelimiter(buffer, indexOf6Delimiter, delimiter);
 
-            double.TryParse(buffer.Slice(indexOf6Delimiter + 1, indexOf7Delimiter - indexOf6Delimiter - 1), out LongitudeEW);
-            
+            double.TryParse(buffer.Slice(indexOf6Delimiter + 1, indexOf7Delimiter - indexOf6Delimiter - 1),
+                out LongitudeEW);
+
             resultLine.Append(LongitudeEW);
             resultLine.Append(delimiter);
 
             var indexOf8Delimiter = GetIndexOfDelimiter(buffer, indexOf7Delimiter, delimiter);
 
-            double.TryParse(buffer.Slice(indexOf7Delimiter + 1, indexOf8Delimiter - indexOf7Delimiter - 1), out LongitudeNS);
-            
+            double.TryParse(buffer.Slice(indexOf7Delimiter + 1, indexOf8Delimiter - indexOf7Delimiter - 1),
+                out LongitudeNS);
+
             resultLine.Append(LongitudeNS);
 
             modifiedStreamWriter.WriteLine(resultLine);
