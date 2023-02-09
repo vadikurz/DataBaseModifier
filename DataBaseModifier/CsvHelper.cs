8764083e426010b0e4159b246c0464eec9f19cb7
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-02-09 16:22:16 +0300

    refactor methods

--- DataBaseModifier/CsvHelper.cs
+++ DataBaseModifier/CsvHelper.cs
@@ -6,105 +6,103 @@ namespace DataBaseModifier;
 
 public class CsvHelper
 {
-    public void ReadAndWrite(GZipStream stream, string originalFileOutputPath, string modifiedFileOutputPath,
+    public async Task ReadAndWrite(GZipStream stream, string originalFileOutputPath, string modifiedFileOutputPath,
         char delimiter)
     {
         CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 
         using var streamReader = new StreamReader(stream);
-        using var modifiedFileStream = File.OpenWrite(modifiedFileOutputPath);
-        using var originalFileStream = File.OpenWrite(originalFileOutputPath);
-        using var modifiedStreamWriter = new StreamWriter(modifiedFileStream);
-        using var originalStreamWriter = new StreamWriter(originalFileStream);
+        await using var modifiedStreamWriter = new StreamWriter(modifiedFileOutputPath);
+        await using var originalStreamWriter = new StreamWriter(originalFileOutputPath);
 
-        string line;
+
         var resultLine = new StringBuilder();
 
-        int MCC;
-        int MNC;
-        int LAC_TAC_NID;
-        int CID;
-        double LongitudeEW;
-        double LongitudeNS;
-
-        while ((line = streamReader.ReadLine()) != null)
+        while (await streamReader.ReadLineAsync() is { } line)
         {
-            originalStreamWriter.WriteLine(line);
+            await originalStreamWriter.WriteLineAsync(line);
 
             resultLine.Clear();
 
-            var firstDelimiter = line.IndexOf(delimiter);
+            var lowRange = line.IndexOf(delimiter);
 
-            if (line[..firstDelimiter] != "GSM")
-            {
+            if (lowRange is -1)
                 continue;
-            }
 
-            ReadOnlySpan<char> buffer = line.AsSpan();
+            if (lowRange != 3 || line[0] != 'G' || line[1] != 'S' || line[2] != 'M')
+                continue;
 
-            var indexOf2Delimiter = GetIndexOfDelimiter(buffer, firstDelimiter, delimiter);
+            var upperRange = line.IndexOf(delimiter, lowRange + 1);
+            
+            if(upperRange is -1)
+                continue;
 
-            int.TryParse(buffer.Slice(firstDelimiter + 1, indexOf2Delimiter - firstDelimiter - 1), out MCC);
+            int.TryParse(line.AsSpan()[++lowRange..upperRange], out var MCC);
 
             resultLine.Append(MCC);
             resultLine.Append(delimiter);
 
-            var indexOf3Delimiter = GetIndexOfDelimiter(buffer, indexOf2Delimiter, delimiter);
+            lowRange = upperRange;
+            upperRange = line.IndexOf(delimiter, lowRange + 1);
+            
+            if(upperRange is -1)
+                continue;
 
-            int.TryParse(buffer.Slice(indexOf2Delimiter + 1, indexOf3Delimiter - indexOf2Delimiter - 1), out MNC);
+            int.TryParse(line.AsSpan()[++lowRange..upperRange], out var MNC);
 
             resultLine.Append(MNC);
             resultLine.Append(delimiter);
 
-            var indexOf4Delimiter = GetIndexOfDelimiter(buffer, indexOf3Delimiter, delimiter);
+            lowRange = upperRange;
+            upperRange = line.IndexOf(delimiter, lowRange + 1);
+            
+            if(upperRange is -1)
+                continue;
 
-            int.TryParse(buffer.Slice(indexOf3Delimiter + 1, indexOf4Delimiter - indexOf3Delimiter - 1),
-                out LAC_TAC_NID);
+            int.TryParse(line.AsSpan()[++lowRange..upperRange], out var LAC_TAC_NID);
 
             resultLine.Append(LAC_TAC_NID);
             resultLine.Append(delimiter);
 
-            var indexOf5Delimiter = GetIndexOfDelimiter(buffer, indexOf4Delimiter, delimiter);
+            lowRange = upperRange;
+            upperRange = line.IndexOf(delimiter, lowRange + 1);
+            
+            if(upperRange is -1)
+                continue;
 
-            int.TryParse(buffer.Slice(indexOf4Delimiter + 1, indexOf5Delimiter - indexOf4Delimiter - 1), out CID);
+            int.TryParse(line.AsSpan()[++lowRange..upperRange], out var CID);
 
             resultLine.Append(CID);
             resultLine.Append(delimiter);
 
-            var indexOf6Delimiter = GetIndexOfDelimiter(buffer, indexOf5Delimiter, delimiter);
+            lowRange = upperRange;
+            upperRange = line.IndexOf(delimiter, lowRange + 1);
+            
+            if(upperRange is -1)
+                continue;
 
-            var indexOf7Delimiter = GetIndexOfDelimiter(buffer, indexOf6Delimiter, delimiter);
+            lowRange = upperRange;
+            upperRange = line.IndexOf(delimiter, lowRange + 1);
+            
+            if(upperRange is -1)
+                continue;
 
-            double.TryParse(buffer.Slice(indexOf6Delimiter + 1, indexOf7Delimiter - indexOf6Delimiter - 1),
-                out LongitudeEW);
+            double.TryParse(line.AsSpan()[++lowRange..upperRange], out var LongitudeEW);
 
             resultLine.Append(LongitudeEW);
             resultLine.Append(delimiter);
 
-            var indexOf8Delimiter = GetIndexOfDelimiter(buffer, indexOf7Delimiter, delimiter);
+            lowRange = upperRange;
+            upperRange = line.IndexOf(delimiter, lowRange + 1);
+            
+            if(upperRange is -1)
+                continue;
 
-            double.TryParse(buffer.Slice(indexOf7Delimiter + 1, indexOf8Delimiter - indexOf7Delimiter - 1),
-                out LongitudeNS);
+            double.TryParse(line.AsSpan()[++lowRange..upperRange], out var LongitudeNS);
 
             resultLine.Append(LongitudeNS);
 
-            modifiedStreamWriter.WriteLine(resultLine);
+            await modifiedStreamWriter.WriteLineAsync(resultLine);
         }
-    }
-
-    private int GetIndexOfDelimiter(ReadOnlySpan<char> buffer, int indexOfPreviousDelimiter, char delimiter)
-    {
-        var index = 0;
-
-        for (int i = indexOfPreviousDelimiter + 1; i < buffer.Length; i++)
-        {
-            if (buffer[i] == delimiter)
-            {
-                index = i;
-                break;
-            }
-        }
-
-        return index;
     }
 }
--- DataBaseModifier/Program.cs
+++ DataBaseModifier/Program.cs
@@ -9,36 +9,18 @@ public class Program
     /// <param name="args">args[0] is the path to file, args[1] is the file name, args[2] is the token</param>
     public static async Task Main(string[] args)
     {
-        string path, fileName, token;
+        var path = Environment.CurrentDirectory;
+        var fileName = "257.csv.gz";
+        var token = "pk.099507f0795cf7dbfb07161f2ad6a4b6";
 
-        var defaultPath = Environment.CurrentDirectory;
-        var defaultFileName = "257.csv.gz";
-        var defaultToken = "pk.f48f741b488ae7c9a645c3fb04171bef";
+        if (args.Length > 0)
+            path = args[0];
 
-        if (args.Length == 0)
-        {
-            path = defaultPath;
-            fileName = defaultFileName;
-            token = defaultToken;
-        }
-        else if (args.Length == 1)
-        {
-            path = args[0];
-            fileName = defaultFileName;
-            token = defaultToken;
-        }
-        else if (args.Length == 2)
-        {
-            path = args[0];
+        if (args.Length > 1)
             fileName = args[1];
-            token = defaultToken;
-        }
-        else
-        {
-            path = args[0];
-            fileName = args[1];
+
+        if (args.Length > 2)
             token = args[2];
-        }
 
         if (!Directory.Exists(path))
         {
@@ -51,7 +33,7 @@ public class Program
             throw new ArgumentException("Expected (*.gz)", nameof(fileName));
         }
 
-        var client = new HttpClient();
+        using var client = new HttpClient();
 
         var url = $"https://opencellid.org/ocid/downloads?token={token}&type=mcc&file={fileName}";
 
@@ -60,7 +42,9 @@ public class Program
 
         var csvHelper = new CsvHelper();
 
-        csvHelper.ReadAndWrite(decompressionStream, Path.Combine(path, fileName[..^".gz".Length]),
-            Path.Combine(path, OutputPrefix + fileName[..^".gz".Length]), ',');
+        await csvHelper.ReadAndWrite(decompressionStream, Path.Combine(path, fileName[..^".gz".Length]),
+           Path.Combine(path, OutputPrefix + fileName[..^".gz".Length]), ',');
+           await csvHelper.ReadAndWrite(decompressionStream, path+"257.csv",
+          path + "modified257.csv", ',');
     }
 }
