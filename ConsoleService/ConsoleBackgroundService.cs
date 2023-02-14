d59d64ed50f1c7ad2a12dd4b6b17fdde0aebc78a
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-02-14 10:50:24 +0300

    add ConsoleBackgroundService

---
+++ ConsoleService/ConsoleBackgroundService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ConsoleService;
+
+public class ConsoleBackgroundService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000, stoppingToken);
+            Console.WriteLine("task running...");
+            
+        }
+    }
+}
---
+++ ConsoleService/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleService;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var service = new ConsoleBackgroundService();
+
+        var cancellationTokenSource = new CancellationTokenSource();
+        var token = cancellationTokenSource.Token;
+
+        await service.StartAsync(token);
+
+        while (Console.ReadKey().Key != ConsoleKey.X)
+        {
+            
+        }
+        
+        cancellationTokenSource.Cancel();
+    }
+}
