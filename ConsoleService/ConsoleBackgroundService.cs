7d12c500ad063326d490c4bd98c41e8fd03dc227
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-02-20 15:30:26 +0300

    add wait for application started

--- ConsoleService/ConsoleBackgroundService.cs
+++ ConsoleService/ConsoleBackgroundService.cs
@@ -6,17 +6,40 @@ namespace ConsoleService;
 public class ConsoleBackgroundService : BackgroundService
 {
     private ILogger<ConsoleBackgroundService> logger;
+    private bool ready;
 
-    public ConsoleBackgroundService(ILogger<ConsoleBackgroundService> logger)
+    public ConsoleBackgroundService(ILogger<ConsoleBackgroundService> logger, IHostApplicationLifetime lifeTime)
     {
         this.logger = logger;
+        lifeTime.ApplicationStarted.Register(() => ready = true);
     }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        while (!ready)
+        {
+            await Task.Delay(1000, stoppingToken);
+        }
+        
         while (!stoppingToken.IsCancellationRequested)
         {
             logger.LogInformation("task running...");
             await Task.Delay(1000, stoppingToken);
         }
+    }
+    
+    private async Task<bool> WaitForAppStartup(IHostApplicationLifetime appLifetime, CancellationToken stoppingToken)
+    {
+        var startedSource = new TaskCompletionSource();
+        var cancelledSource = new TaskCompletionSource();
+
+        await using var reg1 = appLifetime.ApplicationStarted.Register(() => startedSource.SetResult());
+        await using var reg2 = stoppingToken.Register(() => cancelledSource.SetResult());
+
+        var completedTask = await Task.WhenAny(
+            startedSource.Task,
+            cancelledSource.Task).ConfigureAwait(false);
+        
+        return completedTask == startedSource.Task;
     }
 }
