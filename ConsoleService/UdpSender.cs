492e961440168844bdf3f5f4641ab3a79e3bdee2
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-06 22:03:01 +0300

    add UdpSender

---
+++ ConsoleService/UdpSender.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleService;
+
+public class UdpSender : IUdpSender, IDisposable
+{
+    private readonly UdpClient client;
+
+    public UdpSender(string ipAddress, int port)
+    {
+        client = new UdpClient(ipAddress, port);
+    }
+    
+    public async Task SendAsync()
+    {
+        var message = DateTimeOffset.Now.ToString();
+        var data = Encoding.UTF8.GetBytes(message);
+
+        await client.SendAsync(data);
+    }
+
+    public void Dispose() => client.Dispose();
+}
