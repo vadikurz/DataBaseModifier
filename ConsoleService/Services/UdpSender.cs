345bc12fa29e3e18de339aed7ccd2e35b04a7612
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-13 11:19:38 +0300

    add sending data from file

--- ConsoleService/Services/UdpSender.cs
+++ ConsoleService/Services/UdpSender.cs
@@ -9,23 +9,26 @@ namespace ConsoleService.Services;
 public class UdpSender : ApplicationBackgroundService
 {
     private readonly UdpClient _client;
+    private readonly StreamReader _streamReader;
 
-    public UdpSender(IHostApplicationLifetime lifetime, IOptions<UdpSettings> settings) : base(lifetime)
+    public UdpSender(IHostApplicationLifetime lifetime, IOptions<UdpSettings> udpSettings, IOptions<DataSourceSettings> dataSourceSettings) : base(lifetime)
     {
-        _client = new UdpClient(settings.Value.Ip, settings.Value.Port);
+        _client = new UdpClient(udpSettings.Value.Ip, udpSettings.Value.Port);
+        _streamReader = File.OpenText(dataSourceSettings.Value.FilePath);
     }
 
     protected override async Task ExecuteOnApplicationStartAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var message = DateTimeOffset.Now.ToString();
-            
-            var data = Encoding.UTF8.GetBytes(message);
-
-            await _client.SendAsync(data, stoppingToken);
-
-            await Task.Delay(1000, stoppingToken);
+            while (await _streamReader.ReadLineAsync() is { } line)
+            {
+                var data = Encoding.UTF8.GetBytes(line);
+                
+                await _client.SendAsync(data, stoppingToken);
+                
+                await Task.Delay(1000, stoppingToken);
+            }
         }
     }
 }
