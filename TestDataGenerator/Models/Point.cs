1d4656e9ed4e727b8fa385526e0ea860c915b763
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-15 11:14:43 +0300

    add Point class

---
+++ TestDataGenerator/Models/Point.cs
@@ -0,0 +1,28 @@
+﻿namespace TestDataGenerator.Models;
+
+public class Point
+{
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public Point(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+    public static Point Parse(string point)
+    {
+        var delimiter = ",";
+
+        var arrayOfCoordinates = point.Split(delimiter);
+
+        double.TryParse(arrayOfCoordinates[0], out var latitude);
+        double.TryParse(arrayOfCoordinates[1], out var longitude);
+
+        return new Point(latitude, longitude);
+    }
+
+    public override string ToString() => $"{Latitude},{Longitude}";
+}
