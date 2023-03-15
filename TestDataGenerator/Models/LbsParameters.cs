f16b278537b9d65bb1c4711d31069c3833a6301e
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-15 11:15:40 +0300

    add LbsParameters class

---
+++ TestDataGenerator/Models/LbsParameters.cs
@@ -0,0 +1,22 @@
+﻿namespace TestDataGenerator.Models;
+
+public class LbsParameters
+{
+    public int MCC { get; set; }
+    
+    public int MNC { get; set; }
+    
+    public int LAC_TAC_NID { get; set; }
+    
+    public int CID { get; set; }
+
+    public LbsParameters(int mcc, int mnc, int lacTacNid, int cid)
+    {
+        MCC = mcc;
+        MNC = mnc;
+        LAC_TAC_NID = lacTacNid;
+        CID = cid;
+    }
+
+    public override string ToString() => $"{MCC},{MNC},{LAC_TAC_NID},{CID}";
+}
