9d76ef53166ac02b0befd6995760f03c4f0baf65
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-15 11:18:13 +0300

    add CsvWriter

---
+++ TestDataGenerator/Services/CsvWriter.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Text;
+using TestDataGenerator.Models;
+
+namespace TestDataGenerator.Services;
+
+public class CsvWriter
+{
+    public async Task Write(string outputPath,
+        char delimiter, int numberOfSatellites, List<Point> coordinates, List<LbsParameters> lbsParametersList)
+    {
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+        await using var streamWriter = File.CreateText(outputPath);
+
+        var resultLine = new StringBuilder();
+
+        var time = DateTimeOffset.Now;
+
+        for (int i = 0; i < coordinates.Count; i++)
+        {
+            resultLine.Clear();
+
+            time = time.AddSeconds(1);
+
+            resultLine.Append(time);
+            resultLine.Append(delimiter);
+
+            resultLine.Append(coordinates[i]);
+            resultLine.Append(delimiter);
+
+            resultLine.Append(numberOfSatellites);
+            resultLine.Append(delimiter);
+
+            resultLine.Append(lbsParametersList[i]);
+
+            await streamWriter.WriteLineAsync(resultLine);
+        }
+    }
+}
