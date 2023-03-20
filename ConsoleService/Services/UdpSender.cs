7b1e599fad19039f2c6d48f9ddf2645628ae8c33
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-21 00:06:58 +0300

    change constructor, change GetPoints method, add catch block for OperationCanceledException

--- ConsoleService/Services/UdpSender.cs
+++ ConsoleService/Services/UdpSender.cs
@@ -8,22 +8,19 @@ namespace ConsoleService.Services;
 
 public class UdpSender : ApplicationBackgroundService
 {
-    private readonly string _ip;
-    private readonly int _port;
+    private readonly UdpSenderSettings _udpSenderSettings; 
 
     public UdpSender(IHostApplicationLifetime lifetime, IOptions<UdpSenderSettings> udpSenderSettings) : base(lifetime)
     {
-        _ip = udpSenderSettings.Value.Ip;
-        _port = udpSenderSettings.Value.Port;
-
+        _udpSenderSettings = udpSenderSettings.Value;
     }
 
     protected override async Task ExecuteInternalAsync(CancellationToken stoppingToken)
     {
         try
         {
-            using var client = new UdpClient(_ip, _port);
-        
+            using var client = new UdpClient(_udpSenderSettings.Ip, _udpSenderSettings.Port);
+
             var points = GetPoints();
 
             while (!stoppingToken.IsCancellationRequested)
@@ -35,10 +32,14 @@ public class UdpSender : ApplicationBackgroundService
                         var delay = points[i].Time - points[i - 1].Time;
                         await Task.Delay(delay, stoppingToken);
                     }
-                
+
                     await client.SendAsync(points[i].Serialize(), stoppingToken);
                 }
             }
+        }
+        catch (OperationCanceledException exception)
+        {
+            
         }
         catch (Exception exception)
         {
@@ -48,14 +49,14 @@ public class UdpSender : ApplicationBackgroundService
 
     private List<Point> GetPoints()
     {
-        var pointsArray = TestData.Points.Split("\r\n");
-        
-        var points = new List<Point>(pointsArray.Length);
+        var points = new List<Point>();
 
-        foreach (var point in pointsArray)
+        using var reader = new StringReader(TestData.Points);
+
+        while (reader.ReadLine() is {} line)
         {
-            if (Point.TryParse(point, out var resultPoint))
-                points.Add(resultPoint!);
+            if (Point.TryParse(line, out var point))
+                points.Add(point!);
         }
         
         return points;
