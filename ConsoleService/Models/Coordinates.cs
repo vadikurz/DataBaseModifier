ebc59b07b206fa195d24026d172383b7ffd1b39b
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-20 17:41:17 +0300

    add Coordinates record

---
+++ ConsoleService/Models/Coordinates.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace ConsoleService.Models;
+
+public record Coordinates(double Lat, double Lng)
+{
+    public override string ToString()
+    {
+        const char delimiter = ',';
+        
+        var sb = new StringBuilder();
+
+        sb.Append(Lat);
+        sb.Append(delimiter);
+        sb.Append(Lng);
+
+        return sb.ToString();
+    }
+}
