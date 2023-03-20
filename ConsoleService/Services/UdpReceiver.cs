ff790ed6744078708e4d227a482fee323c1abde0
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-21 00:07:55 +0300

    change constructor, change Execute method, add catch bloc for OperationCanceledException

--- ConsoleService/Services/UdpReceiver.cs
+++ ConsoleService/Services/UdpReceiver.cs
@@ -12,34 +12,42 @@ public class UdpReceiver : ApplicationBackgroundService
 {
     private readonly ILogger<UdpReceiver> _logger;
     private readonly LbsService _lbsService;
-    private readonly int _port;
+    private readonly UdpReceiverSettings _settings;
 
     public UdpReceiver(IHostApplicationLifetime lifetime, IOptions<UdpReceiverSettings> settings, ILogger<UdpReceiver> logger, LbsService lbsService) : base(lifetime)
     {
         _logger = logger;
-        _port = settings.Value.Port;
+        _settings = settings.Value;
         _lbsService = lbsService;
     }
-    
+
     protected override async Task ExecuteInternalAsync(CancellationToken stoppingToken)
     {
         try
         {
-            using var client = new UdpClient(_port);
-        
+            using var client = new UdpClient(_settings.Port);
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 var result = await client.ReceiveAsync(stoppingToken);
-            
+
                 var message = Encoding.UTF8.GetString(result.Buffer);
 
                 if (Point.TryParse(message, out var point))
-                    _logger.LogInformation(_lbsService.GetCoordinates(point!).ToString());
+                {
+                    if (_lbsService.TryGetCoordinates(point!, out var coords))
+                    {
+                        _logger.LogInformation(coords.ToString());
+                    }
+                }
             }
         }
-        catch (Exception e)
+        catch (OperationCanceledException exception)
         {
-            Console.WriteLine(e);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
         }
     }
 }
