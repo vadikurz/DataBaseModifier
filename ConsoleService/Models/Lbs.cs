4b476ff86f0252cc333d28d8ef2922f8c9e002b9
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-21 00:00:04 +0300

    rename LbsParameters to Lbs, override ToString method

---
+++ ConsoleService/Models/Lbs.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace ConsoleService.Models;
+
+public record Lbs(int Mcc, int Mnc, int Lac, int Cid)
+{
+    public override string ToString()
+    {
+        const char delimiter = ',';
+
+        var builder = new StringBuilder();
+        
+        builder.Append(Mcc);
+        builder.Append(delimiter);
+        builder.Append(Mnc);
+        builder.Append(delimiter);
+        builder.Append(Lac);
+        builder.Append(delimiter);
+        builder.Append(Cid);
+
+        return builder.ToString();
+    }
+}
--- ConsoleService/Models/LbsParameters.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace ConsoleService.Models;
-
-public record LbsParameters(int Mcc, int Mnc, int Lac, int Cid);
