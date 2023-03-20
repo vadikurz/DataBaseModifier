df70ea4fb3a2e2f7e924ee8ebe133bb8795c6251
mikita_kurzou <Mikita_Kurzou@epam.com>
2023-03-20 20:41:20 +0300

    refactorTryParse method, rename properties

--- ConsoleService/Models/Point.cs
+++ ConsoleService/Models/Point.cs
@@ -3,43 +3,26 @@ using System.Text;
 
 namespace ConsoleService.Models;
 
-public record Point(DateTime TimeStamp, double Lat, double Lng, int NumberOfSatellites, int Mcc, int Mnc, int Lac, int Cid)
+public record Point(DateTime Time, double Lat, double Lng, int Sat, int Mcc, int Mnc, int Lac,
+    int Cid)
 {
     public static bool TryParse(string line, out Point? point)
     {
-        const char delimiter = ',';
-        
-        var style = NumberStyles.Number;
-
         point = default;
-        
-        var parameters = line.Split(delimiter);
 
-        if (!DateTime.TryParse(parameters[0], CultureInfo.InvariantCulture, DateTimeStyles.None,  out var timeStamp))
-            return false;
-        
-        if (!double.TryParse(parameters[1],style, CultureInfo.InvariantCulture , out var latitude))
-            return false;
+        var parameters = line.Split(',');
 
-        if (!double.TryParse(parameters[2], style, CultureInfo.InvariantCulture , out var longitude))
-            return false;
-        
-        if (!int.TryParse(parameters[3], out var numberOfSatellites))
+        if (!DateTime.TryParse(parameters[0], CultureInfo.InvariantCulture, DateTimeStyles.None, out var timeStamp)||
+            !double.TryParse(parameters[1], NumberStyles.Number, CultureInfo.InvariantCulture, out var lat)||
+            !double.TryParse(parameters[2], NumberStyles.Number, CultureInfo.InvariantCulture, out var lng)||
+            !int.TryParse(parameters[3], out var sat)||
+            !int.TryParse(parameters[4], out var mcc)||
+            !int.TryParse(parameters[5], out var mnc)||
+            !int.TryParse(parameters[6], out var lac)||
+            !int.TryParse(parameters[7], out var cid))
             return false;
 
-        if (!int.TryParse(parameters[4], out var mcc))
-            return false;
-
-        if (!int.TryParse(parameters[5], out var mnc))
-            return false;
-
-        if (!int.TryParse(parameters[6], out var lacTacNid))
-            return false;
-
-        if (! int.TryParse(parameters[7], out var cid))
-            return false;
-        
-        point = new Point(timeStamp, latitude, longitude, numberOfSatellites, mcc, mnc, lacTacNid, cid);
+        point = new Point(timeStamp, lat, lng, sat, mcc, mnc, lac, cid);
 
         return true;
     }
@@ -47,16 +30,16 @@ public record Point(DateTime TimeStamp, double Lat, double Lng, int NumberOfSate
     public override string ToString()
     {
         const char delimiter = ',';
-        
+
         var builder = new StringBuilder();
 
-        builder.Append(TimeStamp.ToString(CultureInfo.InvariantCulture));
+        builder.Append(Time.ToString(CultureInfo.InvariantCulture));
         builder.Append(delimiter);
         builder.Append(Lat.ToString(CultureInfo.InvariantCulture));
         builder.Append(delimiter);
         builder.Append(Lng.ToString(CultureInfo.InvariantCulture));
         builder.Append(delimiter);
-        builder.Append(NumberOfSatellites);
+        builder.Append(Sat);
         builder.Append(delimiter);
         builder.Append(Mcc);
         builder.Append(delimiter);
@@ -66,9 +49,8 @@ public record Point(DateTime TimeStamp, double Lat, double Lng, int NumberOfSate
         builder.Append(delimiter);
         builder.Append(Cid);
 
-
         return builder.ToString();
     }
-    
+
     public byte[] Serialize() => Encoding.UTF8.GetBytes(ToString());
 }
